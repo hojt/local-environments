5c32f76bd6ccb03b4817b46cd1af093042e5d08b
Christian Jönsson <christian.cj.jonsson@gmail.com>
2026-08-17 20:14:01 +0000

    fix: validate kustomize app manifests correctly

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,16 +2,25 @@
 
 set -euo pipefail
 
-repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+repo_root="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source "${repo_root}/scripts/config.sh"
 
 cd "${repo_root}"
 
-kubectl \
-  --context "${KUBE_CONTEXT}" \
-  --namespace "${NAMESPACE}" \
-  apply \
-  --dry-run=client \
-  --recursive \
-  --filename "${MANIFEST_DIR}"
+for app_dir in "${MANIFEST_DIR}"/*; do
+  if [[ ! -d "${app_dir}" || ! -f "${app_dir}/kustomization.yaml" ]]; then
+    continue
+  fi
+
+  echo "Validating ${app_dir}"
+
+  kubectl \
+    kustomize "${app_dir}" |
+    kubectl \
+      --context "${KUBE_CONTEXT}" \
+      --namespace "${NAMESPACE}" \
+      apply \
+      --dry-run=client \
+      --filename -
+done
